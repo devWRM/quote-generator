45990106797625e00cdb5c21884b333c1e3f31b8
devWRM <mcwen@comcast.net>
2020-07-07 08:18:50 -0400

    added require relatives to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require "pry"
 require "httparty"
 
-
-
-
+require "./lib/models/quote.rb"
+require "./lib/services/api.rb"
+require "./lib/services/cli.rb"
 
--- lib/services/api.rb
+++ lib/services/api.rb
@@ -1,8 +1,18 @@
 
 
 class API
+    attr_accessor :url
 
+    def initialize(url)
+        @url = url
+    end
 
+    def get_quotes
+        base_url = HTTParty.get(self.url)
+binding.pry
+        
+
+    end
 
 
 
